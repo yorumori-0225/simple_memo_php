af085f5969397d29272be45fc3d650388db074ed
Kobayashi Mio <kobayashi@klar.co.jp>
2021-06-18 17:45:48 +0900

    Add getLoginUserName()

--- common/auth.php
+++ common/auth.php
@@ -16,3 +16,21 @@ function isLogin(): bool
     }
     return false;
 }
+
+/**
+ * ログインしているユーザーの表示用ユーザー名を取得
+ *
+ * @return string
+ */
+function getLoginUserName(): string
+{
+    if (isset($_SESSION['user'])) {
+        $name = $_SESSION['user']['name'];
+
+        if (7 < mb_strlen($name)) {
+            $name = mb_substr($name, 0, 7) . "...";
+        }
+        return $name;
+    }
+    return "";
+}
